57dca2722c78306f7fc03665494ecdfb02da14c6
Tim Wetzel <tim.wetzel@desy.de>
2020-06-24 16:53:56 +0200

    added time measurement to the c++-implementations.

--- add.cpp
+++ add.cpp
@@ -1,6 +1,20 @@
 // This example is taken from https://devblogs.nvidia.com/even-easier-introduction-cuda/
 #include <iostream>
 #include <math.h>
+#include <time.h>
+#include <sys/time.h>
+
+// get_walltime function for time measurement
+double get_walltime_(double* wcTime) {
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  *wcTime = (double)(tp.tv_sec + tp.tv_usec/1000000.0);
+  return 0.0;
+}
+
+void get_walltime(double* wcTime) {
+  get_walltime_(wcTime);
+}
 
 // function to add the elements of two arrays
  void add(int n, float *x, float *y)
@@ -15,6 +29,8 @@ int main(void)
 
     float *x = new float[N];
     float *y = new float[N];
+    double delta, finish, start;
+    double flops, nd;
 
     // initialize x and y arrays on the host
     for (int i = 0; i < N; i++) {
@@ -22,14 +38,29 @@ int main(void)
         y[i] = 2.0f;
     }
 
+    // start time measurement
+    get_walltime(&start);
+
     // Run kernel on 1M elements on the CPU
     add(N, x, y);
+
+    // stop time measurement
+    get_walltime(&finish);
 
     // Check for errors (all values should be 3.0f)
     float maxError = 0.0f;
     for (int i = 0; i < N; i++)
         maxError = fmax(maxError, fabs(y[i]-3.0f));
     std::cout << "Max error: " << maxError << std::endl;
+
+    // calculating time delta and Mflops
+    delta = (finish - start);
+    nd = (double) N;
+    flops = nd/(delta * 1000000.);
+
+    std::cout << ">>>>> finish: " << finish << std::endl;
+    std::cout << ">>>>> delta: " << delta << std::endl;
+    std::cout << ">>>>> Mflops: " << flops << std::endl;
 
     // Free memory
     delete [] x;
--- mult.cpp
+++ mult.cpp
@@ -1,7 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <sys/time.h>
 
+// get_walltime function for time measurement
+double get_walltime_(double* wcTime) {
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  *wcTime = (double)(tp.tv_sec + tp.tv_usec/1000000.0);
+  return 0.0;
+}
+
+void get_walltime(double* wcTime) {
+  get_walltime_(wcTime);
+}
+
+// matrix type definition
 typedef struct {
     int width;
     int height;
@@ -31,6 +45,8 @@ int main(int argc, char** argv)
     int row, col;
     float sum = 0;
     float tmp;
+    double delta, finish, start;
+    double flops, nd;
     Matrix A, B, C;
     A.width = m; A.height = n;
     B.width = m; B.height = n;
@@ -48,7 +64,15 @@ int main(int argc, char** argv)
         //}
     }
 
+    // start time measurement
+    get_walltime(&start);
+
+    // Run kernel on the CPU
     MatMulSerial(A, B, C);
+
+    // stop time measurement
+    get_walltime(&finish);
+
 
     for (row=0; row<C.width; row++) {
         tmp = 0;
@@ -58,6 +82,15 @@ int main(int argc, char** argv)
         sum += tmp;
     }
     printf("Sum of all elements of C is: %f\n", sum);
+
+    // calculating time delta and Mflops
+    delta = (finish - start);
+    nd = (double) n;   
+    flops = (2.*nd*nd*nd-nd*nd)/delta/1000000. ;
+
+    printf(">>>>> finish: %f\n", finish);
+    printf(">>>>> delta:  %f\n", delta );
+    printf(">>>>> Mflops: %f\n", flops );
     
     free(A.elements);
     free(B.elements);
